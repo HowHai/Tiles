866bae623375a07c4245ef9f6972a233d047cb6e
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-19 23:09:30 -0700

    Randomly generate 5 tiles

--- public/modules/tiles/controllers/tiles.js
+++ public/modules/tiles/controllers/tiles.js
@@ -3,24 +3,54 @@
 angular.module('mean.tiles').controller('TilesCtrl', ['$scope', '$http',
   function($scope, $http) {
 
-    $http.get('/tiles', null)
-      .success(function(response) {
-        $scope.tile = response[response.length - 1];
-        console.log(response);
-      })
+    $scope.loadTiles = function() {
+      $http.get('/tiles', null)
+        .success(function(response) {
+          var randomTiles = [];
+          while (randomTiles.length < 5) {
+            var randomNumber = Math.floor(Math.random() * (response.length));
+            var found = false;
+            for (var i = 0; i < randomTiles.length; i++) {
+              if (randomTiles[i] == randomNumber) {found = true; break}
+            }
+            if (!found) randomTiles[randomTiles.length] = randomNumber;
+          }
 
-    // $scope.tiles = [];
-    // var tile = {name: "iPhone5s", content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, eligendi, placeat quae repellat voluptas officiis quisquam quo numquam corrupti odit amet animi tempore consectetur modi dicta fugit voluptatum aspernatur labore.", imgUrl: "http://s1.ibtimes.com/sites/www.ibtimes.com/files/styles/v2_article_large/public/2013/08/26/iphone-5s.JPG"};
+          console.log(randomTiles);
+          console.log(response.length);
 
-    // $scope.tiles.push(tile);
-    // $scope.tiles.push(tile);
-    // $scope.tiles.push(tile);
-    // $scope.tiles.push(tile);
-    // $scope.tiles.push(tile);
+          $scope.tileMain = response[randomTiles[0]];
+          $scope.tileLeft = response[randomTiles[1]];
+          $scope.tileRight = response[randomTiles[2]];
+          $scope.tileUp = response[randomTiles[3]];
+          $scope.tileDown = response[randomTiles[4]];
+
+          $scope.horizontal = [];
+          $scope.horizontal.push($scope.tileLeft, $scope.tileMain, $scope.tileRight);
+          console.log($scope.horizontal);
+        });
+    }
+
+    $scope.moveUp = function() {
+      // $scope.tileMain = $scope.tileUp;
+      $scope.loadTiles();
+    }
+
+    $scope.moveDown = function() {
+      $scope.tileMain = $scope.tileDown; 
+    }
+
+    $scope.moveLeft = function() {
+      $scope.tileMain = $scope.tileLeft; 
+    }
+
+    $scope.moveRight = function() {
+      $scope.tileMain = $scope.tileRight; 
+    }
 
     // Create a random tile and save to database
     $scope.createTile = function() {
-      $http.get('/tiles', null)
+      $http.post('/tiles', null)
         .success(function(response) {
 
           // Assigns created tile object to $scope.tile
